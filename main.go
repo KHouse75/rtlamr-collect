c674b604e21403ae57afbabb037f4fcef644c8f7
KHouse75 <24616556+KHouse75@users.noreply.github.com>
2020-09-13 03:00:08 -0400

    Update main.go

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ import (
 	"time"
 
 	client "github.com/influxdata/influxdb1-client/v2"
-	"github.com/eclipse/paho.mqtt.golang"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/pkg/errors"
 )
 
